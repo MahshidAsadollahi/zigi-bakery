0bee89e36f3eafb9cf1845ba2306a9c8cc77e15b
Mahshid Asadollahimahboubian <sarahcummings@Mahshids-MacBook-Pro.local>
2024-08-01 14:32:16 +0200

    undo order changes

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -6,57 +6,25 @@ import mongoose from "mongoose";
 import { getServerSession } from "next-auth/next";
 
 export async function GET(req) {
-  try {
-    // Ensure MongoDB connection is established
-    if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-    }
+  mongoose.connect(process.env.MONGO_URL);
 
-    // Get session and user information
-    const session = await getServerSession(authOptions);
-    const userEmail = session?.user?.email;
-    const admin = await isAdmin();
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  const admin = await isAdmin();
 
-    // Parse the request URL
-    const url = new URL(req.url);
-    const _id = url.searchParams.get("_id");
-    if (_id) {
-      const order = await Order.findById(_id);
-      if (order) {
-        return new Response(JSON.stringify(order), {
-          headers: { "Content-Type": "application/json" },
-          status: 200,
-        });
-      } else {
-        return new Response("Order not found", { status: 404 });
-      }
-    }
-
-    // Return all orders if user is admin
-    if (admin) {
-      const orders = await Order.find();
-      return new Response(JSON.stringify(orders), {
-        headers: { "Content-Type": "application/json" },
-        status: 200,
-      });
-    }
-
-    // Return user-specific orders
-    if (userEmail) {
-      const userOrders = await Order.find({ userEmail });
-      return new Response(JSON.stringify(userOrders), {
-        headers: { "Content-Type": "application/json" },
-        status: 200,
-      });
-    }
-
-    // Handle cases where user is not authorized
-    return new Response("Unauthorized", { status: 401 });
-  } catch (error) {
-    console.error("Error fetching orders:", error);
-    return new Response("Internal Server Error", { status: 500 });
+  const url = new URL(req.url);
+  const _id = url.searchParams.get('_id');
+  if (_id) {
+    return Response.json( await Order.findById(_id) );
   }
+
+
+  if (admin) {
+    return Response.json( await Order.find() );
+  }
+
+  if (userEmail) {
+    return Response.json( await Order.find({userEmail}) );
+  }
+
 }
