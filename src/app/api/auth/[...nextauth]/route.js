445062731a2ff56745e473bafdbbfec91d82f294
Mahshid Asadollahimahboubian <sarahcummings@mahshids-mbp.speedport.ip>
2024-08-05 16:23:35 +0200

    fixes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 import clientPromise from "@/libs/mongoConnect";
-import {UserInfo} from "@/app/models/UserInfo";
+
 import bcrypt from 'bcryptjs';
 import * as mongoose from "mongoose";
 import {User} from "@/app/models/User";
-import NextAuth, {getServerSession} from "next-auth";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
@@ -41,18 +41,7 @@ export const authOptions = {
   ],
 };
 
-export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({email:userEmail});
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
-}
+
 
 const handler = NextAuth(authOptions);
 
